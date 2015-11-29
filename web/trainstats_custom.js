236c2c568f97eafe9c95e2dff5d195e6167ce457
Phil Everson <phil@phileverson.com>
2015-11-29 10:24:29 -0500

    Added share buttons, finished top 10 / worst 10, added auto scroll, blank sections for about train stats, and contact / copy right stuff.

--- web/trainstats_custom.js
+++ web/trainstats_custom.js
@@ -9,60 +9,64 @@ trainData[0] = ['Train', 'Ontime or less than 5 minutes late', 'between 5 and 30
 
 
 function drawStackedTripDelaysByTrain() {
-  console.log(trainData);
-var data = google.visualization.arrayToDataTable(trainData);
+	var data = google.visualization.arrayToDataTable(trainData);
 
-      var options = {
-        isStacked: 'percent',
-        height: 450,
-        backgroundColor: '#f8f8f8',
-        legend: {position: 'top', alignment: 'center', maxLines: 3},
-        hAxis: {
-          title: 'Train Number',
-        },
-        vAxis: {
-          title: 'Percentage of Trips'
-        },
-        chartArea:{left:60,top:30,width:"100%",height:350},
-        colors: ['#FFD569', '#FEBA0D', '#A07300'],
-  
-      fontName: 'Montserrat',
-      fontSize: 14,
+	var options = {
+		isStacked: 'percent',
+		height: 450,
+		backgroundColor: '#f8f8f8',
+		legend: {position: 'top', alignment: 'center', maxLines: 3},
+		hAxis: {
+			title: 'Train Number',
+		},
+		vAxis: {
+			title: 'Percentage of Trips'
+		},
+		chartArea:{left:60,top:30,width:"100%",height:350},
+		colors: ['#FFD569', '#FEBA0D', '#A07300'],
 
-      };
+		fontName: 'Montserrat',
+		fontSize: 14,
 
-      var dailyTripDelaysChart = new google.visualization.ColumnChart(document.getElementById('dailyTripDelaysGraph'));
-      
-      dailyTripDelaysChart.draw(data, options);
+	};
+
+	var dailyTripDelaysChart = new google.visualization.ColumnChart(document.getElementById('dailyTripDelaysGraph'));
+
+	dailyTripDelaysChart.draw(data, options);
 }
 
 function addDataCallback(e) {
-  trainData.push(['Train ' + e['train_number'], e[0], e[10], e[30], '']);
+	trainData.push(['Train ' + e['train_number'], e[0], e[10], e[30], '']);
 }
 
 function arrangeTrainBucketData(originStation, destStation) {
-  var allTrains = getTrainsByRoute(originStation, destStation);
-  var deferred = [];
-  for (var i = 0; i < allTrains.length; i++) {
-    deferred.push(getDelaysAtStation(allTrains[i], destStation, 180, addDataCallback));
-  }
-  $.when.apply($, deferred).then(function() {
-    drawStackedTripDelaysByTrain();
-  });
+	var allTrains = getTrainsByRoute(originStation, destStation);
+	var deferred = [];
+	for (var i = 0; i < allTrains.length; i++) {
+		deferred.push(getDelaysAtStation(allTrains[i], destStation, 180, addDataCallback));
+	}
+	$.when.apply($, deferred).then(function() {
+		drawStackedTripDelaysByTrain();
+	});
 }
 
 
 // From the same stackoverflow as always: http://stackoverflow.com/a/901144
 function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 function toTitleCase(str)
 {
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
 }
 
 
@@ -76,17 +80,17 @@ function loadTripDelaysByTrain(originStation, destStation) {
 }
 
 function loadTripFastFactsStats(originStation, destStation) {
-  var numTrains = getTrainsByRoute(originStation, destStation).length;
-  $('.num-trains-run-cta').text(numTrains + ' trains');
-  $('.num-trains-run-desc').text('run between ' + toTitleCase(originStation) + ' and ' + toTitleCase(destStation));
+	var numTrains = getTrainsByRoute(originStation, destStation).length;
+	$('.num-trains-run-cta').text(numTrains + ' trains');
+	$('.num-trains-run-desc').text('run between ' + toTitleCase(originStation) + ' and ' + toTitleCase(destStation) + ' most days');
 
-  getAverageDelays(originStation,destStation,180,10, function(e){
-    $('.num-ten-or-more-late-cta').text(Math.round(e) + '%');
-  });
+	getAverageDelays(originStation,destStation,180,10, function(e){
+		$('.num-ten-or-more-late-cta').text(Math.round(e) + '%');
+	});
 
-  getAverageDelays(originStation,destStation,180,30, function(e){
-    $('.num-thirty-or-more-late-cta').text(Math.round(e) + '%');
-  });
+	getAverageDelays(originStation,destStation,180,30, function(e){
+		$('.num-thirty-or-more-late-cta').text(Math.round(e) + '%');
+	});
 
 
   //crappy height way
@@ -97,40 +101,60 @@ function loadTripFastFactsStats(originStation, destStation) {
 }
 
 function loadAllStations() {
-  var originStationMenu = $('#originSelect');
-  var destStationMenu = $('#destinationSelect');
+	var originStationMenu = $('#originSelect');
+	var destStationMenu = $('#destinationSelect');
 
-  var allStations = train_info.all_stops;
+	var allStations = train_info.all_stops;
 
-  for (var i = 0; i < allStations.length; i++) {
-    originStationMenu.append("<option value=\"" + allStations[i] + "\">" + allStations[i] + "</option>");
-    destStationMenu.append("<option value=\"" + allStations[i] + "\">" + allStations[i] + "</option>");
-  };
+	for (var i = 0; i < allStations.length; i++) {
+		originStationMenu.append("<option value=\"" + allStations[i] + "\">" + allStations[i] + "</option>");
+		destStationMenu.append("<option value=\"" + allStations[i] + "\">" + allStations[i] + "</option>");
+	};
 
-//  init for auto completing
-  $(".select2").select2();
+	//  init for auto completing
+	$(".select2").select2({});
 
-  renderTrainDetails();
+	renderTrainDetails();
 }
 
 function renderTrainDetails() {
-  var originStation = getParameterByName('originSelect');
-  var destStation = getParameterByName('destinationSelect');
+	var originStation = getParameterByName('originSelect');
+	var destStation = getParameterByName('destinationSelect');
 
-  if(originStation.length > 0 && destStation.length > 0) {
+	if(originStation.length > 0 && destStation.length > 0) {
+		$('[name=originSelect] option[value="' + originStation + '"]').prop('selected','selected');
+		$('[name=destinationSelect] option[value="' + destStation + '"]').prop('selected',true);
 
-    $('[name=originSelect] option[value="' + originStation + '"]').prop('selected','selected');
-    $('[name=destinationSelect] option[value="' + destStation + '"]').prop('selected',true);
+		$('#originSelect').val(originStation).trigger('change');
+		$('#destinationSelect').val(destStation).trigger('change');
 
-$('#originSelect').val(originStation).trigger('change');
-$('#destinationSelect').val(destStation).trigger('change');
+		$('#statsBestWorst').hide();
+		$('#statsTrainRoute').show();
+		$('#dailyTripDelays').show();
+		loadTripDelaysByTrain(originStation, destStation);
+		loadTripFastFactsStats(originStation, destStation);
+		scrollToAnchor('searchBox');
+	}
+	else {
+		loadTop10Slow();
+	}
+}
 
-    $('#statsBestWorst').hide();
-    $('#statsTrainRoute').show();
-    $('#dailyTripDelays').show();
-    loadTripDelaysByTrain(originStation, destStation);
-    loadTripFastFactsStats(originStation, destStation);
-  }
+function loadTop10Fast() {
+	getBest(10, function(bestTrains){
+		for (var j = 0; j < bestTrains.length; j++) {
+			$('#bestTrainsTableBody').append('<tr><td>Train #' + bestTrains[j]['train'] + '</br><span class="stats-train-route">' + bestTrains[j]['first_station'] + ' - ' + bestTrains[j]['last_station'] + '</span</td><td>' + Math.round(bestTrains[j]['average']) + ' minutes</td></tr>');
+		};
+	})
+}
+
+function loadTop10Slow() {
+	getWorst(10, function(worstTrains){
+		for (var i = 0; i < worstTrains.length; i++) {
+			$('#worstTrainsTableBody').append('<tr><td>Train #' + worstTrains[i]['train'] + '</br><span class="stats-train-route">' + worstTrains[i]['first_station'] + ' - ' + worstTrains[i]['last_station'] + '</span</td><td>' + Math.round(worstTrains[i]['average']) + ' minutes</td></tr>');
+		};
+		loadTop10Fast();
+	})
 }
 
 
@@ -139,5 +163,5 @@ $('#destinationSelect').val(destStation).trigger('change');
 // *************************************************************
 
 $( document ).ready(function() {
-  dataInit(loadAllStations);
+	dataInit(loadAllStations);
 });
